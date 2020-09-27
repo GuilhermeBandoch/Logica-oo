b5bdd566dd2e51be39caf156aba6c90d1309963f
GUILHERME BANDOCH <68199@proway.treina>
2020-09-27 17:50:41 -0300

    metodo construtor com parametros

--- Zoologico/src/br/com/logica/zoologico/principal/Main.java
+++ Zoologico/src/br/com/logica/zoologico/principal/Main.java
@@ -6,15 +6,13 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// Criando uma nova instancia do objeto
-		Animal a1 = new Animal();
+		Animal gato = new Animal("Jojo todinho","Gato",1);
 		
-		a1.setNome("Jojo todinho");
-		a1.setEspecie("Gato");
-		a1.setIdade (1);
+		gato.emitirSom();
 		
-		a1.emitirSom();
-		System.out.println("Nome do animal " + a1.getNome());
-		if(a1.isAdult()) {
+		System.out.println("Olá, seu animal é um " + gato.getEspecie()+", o nome dele é " + gato.getNome() + "e tem "+gato.getIdade()+" anos");
+		
+		if(gato.isAdult()) {
 			System.out.println("É adulto");
 		}else {
 			System.out.println("Não é adulto");
@@ -22,18 +20,17 @@ public class Main {
 		
 		System.out.println("---------------------------------");
 		// Criando uma nova instancai do objeto
-		Animal a2 = new Animal();
+		Animal cachorro = new Animal("Spike","cachorro",3);
 		
 		//atribuindo os valores as propriedade
-		a2.setNome("Spike");
-		a2.setEspecie("Cachorro");
-		a2.setIdade(3);
 		
-		a2.emitirSom();
 		
-		System.out.println("Ola o nome do meu animal é " + a2.getNome());
+		cachorro.emitirSom();
 		
-		if(a2.isAdult()) {
+		System.out.println("Olá, seu animal é um " + cachorro.getEspecie()+", o nome dele é " + cachorro.getNome() + "e tem "+cachorro.getIdade()+" anos");
+
+		
+		if(cachorro.isAdult()) {
 			System.out.println("É adulto");
 		}else {
 			System.out.println("Não é adulto");
