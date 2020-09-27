8564876d8ca1760a31edb7cb16575869585ce09b
GUILHERME BANDOCH <68199@proway.treina>
2020-09-27 16:48:23 -0300

    Utilizando metodos acessores e qualificadores

--- Zoologico/src/br/com/logica/zoologico/classes/Animal.java
+++ Zoologico/src/br/com/logica/zoologico/classes/Animal.java
@@ -5,8 +5,29 @@ public class Animal {
 	
 	// atributos ou propriedade do objeto
 	// nesse momento as propiedades são públicas
-	 public String nome;
-	 public String especie;
-	 public int idade;
+	 private String nome;
+	 private String especie;
+	 private int idade;
 	
+	 public void setNome(String nome) {
+		 this.nome = nome;
+	 }
+	 public String getNome() {
+		 return nome;
+	 }
+	 
+	 public void setEspecie(String especie) {
+		 this.especie = especie;
+	 }
+	 public String getEspecie() {
+		 return especie;
+	 }
+	 
+	 public void setIdade(int idade) {
+		 this.idade = idade;
+	 }
+	 public int getIdade() {
+		 return idade;
+	 }
+	 
 }
--- Zoologico/src/br/com/logica/zoologico/principal/Main.java
+++ Zoologico/src/br/com/logica/zoologico/principal/Main.java
@@ -8,17 +8,19 @@ public class Main {
 		// Criando uma nova instancia do objeto
 		Animal a1 = new Animal();
 		
-		a1.nome = "Jojo todinho";
-		a1.especie = "Gato";
-		a1.idade = 1;
+		a1.setNome("Jojo todinho");
+		a1.setEspecie("Gato");
+		a1.setIdade (1);
 		
 		// Criando uma nova instancai do objeto
 		Animal a2 = new Animal();
 		
 		//atribuindo os valores as propriedade
-		a2.nome = "Spike";
-		a2.especie = "Cachorro";
-		a2.idade = 3;
+		a2.setNome("Spike");
+		a2.setEspecie("Cachorro");
+		a2.setIdade(3);
+		
+		System.out.println("Ola o nome do meu animal é " + a2.getNome());
 	}
 
 }
